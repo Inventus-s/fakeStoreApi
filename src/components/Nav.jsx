8cb2077267414ebb1283d2b24bdd49bfbeb02a80
Inventus-s <handle.extras@gmail.com>
2024-03-05 01:17:02 +0530

    distinct-categories added

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,26 +1,33 @@
 import React from 'react';
+import { useContext } from 'react';
+import { ProductContext } from './utils/Context';
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
+  const [products] = useContext(ProductContext);
+
+  let distinct_category = products && products.reduce((acc, cv) =>[...acc,cv.category],[]);
+  distinct_category = [...new Set(distinct_category)];
+
+  const color = ()=>{
+    return `rgba(${(Math.random()*255).toFixed()},${(Math.random()*255).toFixed()},${(Math.random()*255).toFixed()})`
+  };
+  
     return (
         <nav className="w-[15%] h-full bg-zinc-50 flex flex-col items-center pt-5">
           <a className='py-2 px-5 border rounded border-blue-200 text-blue-300'
            href='/create'>Add New Products</a>
           <hr className='w-[80%] my-3'></hr>
           <h1 className="text-2xl mb-3 w-[80%]">Category Filter</h1>
-          <ul className="w-[80%]">
-            <li className="flex items-center mb-3">
-            <span className="rounded-full mr-2 w-[15px] h-[15px] bg-blue-100"></span>{" "}
-            cat 1
-            </li>
-            <li className="flex items-center mb-3">
-            <span className="rounded-full mr-2 w-[15px] h-[15px] bg-red-100"></span>{" "}
-            cat 1
-            </li>
-            <li className="flex items-center mb-3">
-            <span className="rounded-full mr-2 w-[15px] h-[15px] bg-green-100"></span>{" "}
-            cat 1
-            </li>
-          </ul>
+          <div className="w-[80%]">
+          {distinct_category.map((c,i) => (
+            <Link to={`/?category=${c}`} key={i} className="flex items-center mb-3">
+            <span style={{backgroundColor : color()}} className="rounded-full mr-2 w-[15px] h-[15px]"></span>{c}
+            </Link>
+          )
+          )}
+            
+          </div>
         </nav>
     );
 }
